7cbc1960b0a58203f8311e0bab9dd8336b1a6579
Gabriel S. Pires <gabriel.slpires@gmail.com>
2024-05-24 21:10:24 -0300

    don't export PneumatronData

--- R/PneumatronData.R
+++ R/PneumatronData.R
@@ -8,24 +8,6 @@
 #' @slot data data.table, represents the data stored in a data.table.
 #' @slot file_path Character vector, represents the file path for the data file.
 #' @slot data_format Character vector, specifies the Pneumatron data format of the data (e.g., V2, V3, V4).
-#' @examples
-#' # To create a PneumatronData object, first ensure your data.frame includes
-#' # the required columns: id, measure, log_line, pressure, and datetime. Here's an example:
-#' df <- data.frame(
-#'   id = c(1, 1, 1, 1, 1),
-#'   measure = c(1, 2, 3, 4, 5),
-#'   log_line = c(1, 2, 3, 4, 5),
-#'   pressure = c(100, 40, 40.7, 41.5, 41.9),
-#'   datetime = as.POSIXct(c(
-#'     "2024-05-13 13:54:26",
-#'     "2024-05-13 13:54:26",
-#'     "2024-05-13 13:54:27",
-#'     "2024-05-13 13:54:27",
-#'     "2024-05-13 13:54:28"
-#'   ))
-#' )
-#' # Now, create the PneumatronData object.
-#' pneumatron_data <- PneumatronData(data = df, file_path = "file_path", data_format = "data_format")
 #' @rdname PneumatronData
 setClass(
   Class = "PneumatronData",
@@ -40,7 +22,6 @@ setClass(
 #' @param data data.table, represents the data stored in a data.table.
 #' @param file_path Character vector, represents the file path for the data file.
 #' @param data_format Character vector, specifies the Pneumatron data format of the data (e.g., V2, V3, V4).
-#' @export
 PneumatronData <- function(data, file_path = NA_character_, data_format = NA_character_) {
   new("PneumatronData", data = data, file_path = file_path, data_format = data_format)
 }
@@ -72,3 +53,22 @@ setMethod("initialize", "PneumatronData", function(.Object, data, file_path = NA
 
   return(.Object)
 })
+
+#' Get data from PneumatronData object
+#'
+#' This method retrieves the data stored in a PneumatronData object.
+#'
+#' @param x A PneumatronData object.
+#' @return A data.table containing the data stored in the PneumatronData object.
+#' @examples
+#' \dontrun{
+#' pneumatron_data <- PneumatronData(data = df)
+#' data <- get_data(pneumatron_data)
+#' }
+#' @rdname get_data
+#' @export
+setGeneric("get_data", function(x) standardGeneric("get_data"))
+#' @rdname get_data
+#' @export
+setMethod("get_data", "PneumatronData", function(x) x@data)
+
